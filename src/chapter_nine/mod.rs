a80ef5aae169c9e67d28f39addf62c9d9781f803
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-23 13:44:57 +0200

    Addding chapter_nine examples. 20250723 1344CET SDvW

--- src/chapter_nine/mod.rs
+++ src/chapter_nine/mod.rs
@@ -1,3 +1,5 @@
+
+
 pub fn hello() {
     println!("Hello from chapter nine!");
 }
@@ -11,4 +13,28 @@ pub fn chapter_nine_paragraph_913_2() {
     let mut iterator: std::iter::Rev<std::slice::Iter<'_, i32>> = big_vec.iter().rev();
     assert_eq!(iterator.next(), Some(&5));
     assert_eq!(iterator.next(), Some(&6));
+}
+
+pub fn chapter_nine_paragraph_913_3(){
+    let mut big_vec = vec![6; 10];
+    big_vec.push(5);
+    println!("{:?}", big_vec.iter().rev().any(|&number| number == 5)); 
+}
+
+pub fn chapter_nine_paragraph_913_4() {
+    // This function is a placeholder for the paragraph 913.4 content.
+    let mut big_vec = vec![6; 10];
+    big_vec.push(5);
+
+    let mut num_loops = 0;
+    // 20250723 1340CET SDvW testing reverted iterator.
+    let mut big_iter = big_vec.into_iter().rev();
+    loop {
+        num_loops +=1;
+        println!("{:?}", num_loops);
+        if big_iter.next() == Some(5) {
+            break;
+        }
+    }
+    println!("Number of loops: {num_loops}");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ pub mod chapter_nine;
 fn main() {
     println!("Hello, world!");
     chapter_nine::chapter_nine_paragraph_913_2();
+    chapter_nine::chapter_nine_paragraph_913_3();
+    chapter_nine::chapter_nine_paragraph_913_4();
 }
